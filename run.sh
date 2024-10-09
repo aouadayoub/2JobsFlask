68358979ab9618bb8d106ee484cbd754a1c2885c
Ayoub <aouadayoub2003@gmail.com>
2024-10-09 17:01:37 +0100

    adding a health check

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Wait for a few seconds to ensure services like MongoDB and Redis are available
-echo "Waiting for MongoDB and Redis to be available..."
-sleep 4
-
-# Start the Flask app with Gunicorn
-echo "Starting Flask app with Gunicorn..."
-gunicorn app:app --bind 0.0.0.0:${PORT} --workers 4
